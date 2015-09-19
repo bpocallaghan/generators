f3b1504a7d50215bdb079a5e6fedc7bcdcb794a9
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2015-09-19 09:21:10 +0200

    Add replace 'class' placeholder for pivot

--- src/Commands/MigrationPivotCommand.php
+++ src/Commands/MigrationPivotCommand.php
@@ -3,7 +3,6 @@
 namespace Bpocallaghan\Generators\Commands;
 
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 
 class MigrationPivotCommand extends GeneratorCommand
 {
@@ -125,6 +124,20 @@ class MigrationPivotCommand extends GeneratorCommand
         $stub = str_replace(['{{tableOne}}', '{{tableTwo}}'], array_merge(array_map('str_plural', $tables), $tables), $stub);
 
         return $this;
+    }
+
+    /**
+     * Replace the class name for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $name
+     * @return string
+     */
+    protected function replaceClass($stub, $name)
+    {
+        $class = str_replace($this->getNamespace($name).'\\', '', $name);
+
+        return str_replace('{{class}}', $class, $stub);
     }
 
     /**
