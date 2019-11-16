0c6ab34c1e9c66055afb67bb55fc1de58642c26b
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2019-11-16 15:06:35 +0200

    Tests - generate:view

--- tests/GenerateFilesTest.php
+++ tests/GenerateFilesTest.php
@@ -15,22 +15,6 @@ class GenerateFilesTest extends TestCase
     }
 
     /** @test */
-    public function generate_view()
-    {
-        $this->artisan('generate:view view');
-        $this->assertFileExists('resources/views/view.blade.php');
-
-        $this->artisan('generate:view foo.bar');
-        $this->assertFileExists('resources/views/foo/bar.blade.php');
-
-        $this->artisan('generate:view foo --stub=view_show');
-        $this->assertFileExists('resources/views/foo.blade.php');
-
-        $this->artisan('generate:view foo --name=foo_bar');
-        $this->assertFileExists('resources/views/foo/foo_bar.blade.php');
-    }
-
-    /** @test */
     public function generate_model()
     {
         $this->artisan('generate:model test');
---
+++ tests/GenerateViewTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Bpocallaghan\Generators\Tests;
+
+class GenerateViewTest extends TestCase
+{
+    /** @test */
+    public function generate_view()
+    {
+        $this->artisan('generate:view view');
+        $this->assertFileExists('resources/views/view.blade.php');
+
+        $this->artisan('generate:view foo.bar');
+        $this->assertFileExists('resources/views/foo/bar.blade.php');
+
+        $this->artisan('generate:view foo --stub=view_show');
+        $this->assertFileExists('resources/views/foo.blade.php');
+
+        $this->artisan('generate:view foo --name=foo_bar');
+        $this->assertFileExists('resources/views/foo/foo_bar.blade.php');
+    }
+}
