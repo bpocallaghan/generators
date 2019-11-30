3ae48a7d98eef253e8b32ba2df3b17393bc76847
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2019-11-30 11:24:48 +0200

    Tests - generate:event and generate:listener

---
+++ tests/GenerateEventsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bpocallaghan\Generators\Tests;
+
+class GenerateEventsTest extends TestCase
+{
+    /** @test */
+    public function generate_events()
+    {
+        $this->artisan('generate:event InvoiceWasPaid');
+        $this->assertFileExists('app/Events/InvoiceWasPaid.php');
+
+        $this->artisan('generate:listener NotifyUserAboutPayment --event=InvoiceWasPaid');
+        $this->assertFileExists('app/Listeners/NotifyUserAboutPayment.php');
+    }
+}
