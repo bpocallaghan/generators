7c95f6e4240d5a1de37842ede4569099952d8d03
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2020-10-06 05:57:55 +0200

    Test stub - refresh database trait and sort imports

--- tests/TestCommandTest.php
+++ tests/TestCommandTest.php
@@ -7,13 +7,13 @@ class TestCommandTest extends TestCase
     /** @test */
     public function generate_test()
     {
-        $this->artisan('generate:test UserCanLogin');
-        $this->assertFileExists('tests/Feature/UserCanLoginTest.php');
+        $this->artisan('generate:test Controllers/UsersController');
+        $this->assertFileExists('tests/Feature/Controllers/UsersControllerTest.php');
 
-        $this->artisan('generate:test Post --unit');
-        $this->assertFileExists('tests/Unit/PostTest.php');
+        $this->artisan('generate:test Models/Post --unit');
+        $this->assertFileExists('tests/Unit/Models/PostTest.php');
 
-        $this->artisan('generate:test Billing/InvoiceTest --unit');
-        $this->assertFileExists('tests/Unit/Billing/InvoiceTest.php');
+        $this->artisan('generate:test Traits/InvoiceTest --unit');
+        $this->assertFileExists('tests/Unit/Traits/InvoiceTest.php');
     }
 }
