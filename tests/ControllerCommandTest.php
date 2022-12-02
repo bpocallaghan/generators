2866f7ad4c82899045434b14618a762e4646ebfa
bpocallaghan <bpocallaghan@gmail.com>
2022-12-02 15:51:38 +0000

    refactor controller command test

--- tests/ControllerCommandTest.php
+++ tests/ControllerCommandTest.php
@@ -9,16 +9,28 @@ class ControllerCommandTest extends TestCase
     {
         $this->artisan('generate:controller test');
         $this->assertFileExists('app/Http/Controllers/TestController.php');
+    }
 
+    /** @test */
+    public function uppercase_file_name()
+    {
         $this->artisan('generate:controller foo.bar');
         $this->assertFileExists('app/Http/Controllers/Foo/BarController.php');
 
         $this->artisan('generate:controller fooBar');
         $this->assertFileExists('app/Http/Controllers/FooBarController.php');
+    }
 
+    /** @test */
+    public function option_plain_stub()
+    {
         $this->artisan('generate:controller plain --plain');
         $this->assertFileExists('app/Http/Controllers/PlainController.php');
+    }
 
+    /** @test */
+    public function do_not_include_controller_suffix_if_already_in_name()
+    {
         $this->artisan('generate:controller SuffixController');
         $this->assertFileExists('app/Http/Controllers/SuffixController.php');
     }
